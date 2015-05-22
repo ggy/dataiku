c255004dd724faa0f4a7b2b206036de08a42ae48
glagasse <mail_ggy@yahoo.fr>
2015-05-22 10:49:21 +0200

    Gestion du fichier

--- Question 1.R
+++ Question 1.R
@@ -1,3 +1,7 @@
 census_income_learn <- read.table("~/Data/dataiku/census_income_learn.csv", sep=";", quote="\"")
 
 library('ggplot2')
+Age = census_income_learn$V1
+qplot(Age)
+
+summary(census_income_learn)
